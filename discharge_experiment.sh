a78ff7ffcc373928a82ec73f5ad47e0308b7f340
crystal0112 <crystal@sktai.io>
2022-05-07 23:06:58 +0900

    add experiment sh file

--- discharge_experiment.sh
+++ discharge_experiment.sh
@@ -1,6 +1,6 @@
 for train_batch_size in 16 32;  do
-    for ACCUMULATION_STEP in 1 2 4;  do
-        for max_grad_norm in 1 3 5;  do
+    for ACCUMULATION_STEP in 1 2;  do
+        for max_grad_norm in 1 3;  do
             for learning_rate in 0.00005 0.00002; do
                 python ./modeling/training_clinical_bert.py \
                   --qtype discharge \
@@ -9,13 +9,13 @@ for train_batch_size in 16 32;  do
                   --data_file_path ./data/discharge/ \
                   --bert_model ./pretraining/ \
                   --max_seq_len 512 \
-                  --output_file_path ./discharge_model_experiment \
+                  --output_file_path ./experiment/discharge_model_experiment \
                   --cuda_num 1 \
                   --train_batch_size ${train_batch_size} \
                   --learning_rate ${learning_rate} \
                   --gradient_accumulation_steps ${ACCUMULATION_STEP} \
                   --max_grad_norm ${max_grad_norm} \
-                  --num_train_epochs 1
+                  --num_train_epochs 3
             done
         done
     done
--- readmission_experiment.sh
+++ readmission_experiment.sh
@@ -1,21 +1,21 @@
 for train_batch_size in 16 32;  do
-    for ACCUMULATION_STEP in 1 2 4;  do
-        for max_grad_norm in 1 3 5;  do
+    for ACCUMULATION_STEP in 1 2;  do
+        for max_grad_norm in 1 3;  do
             for learning_rate in 0.00005 0.00002; do
                 python ./modeling/training_clinical_bert.py \
-                  --qtype discharge \
+                  --qtype readmission \
                   --do_train True \
                   --do_test True \
                   --data_file_path ./data/3days/ \
                   --bert_model ./pretraining/ \
                   --max_seq_len 512 \
-                  --output_file_path ./readmission_model_experiment \
+                  --output_file_path ./experiment/readmission_model_experiment \
                   --cuda_num 0 \
                   --train_batch_size ${train_batch_size} \
                   --learning_rate ${learning_rate} \
                   --gradient_accumulation_steps ${ACCUMULATION_STEP} \
                   --max_grad_norm ${max_grad_norm} \
-                  --num_train_epochs 1
+                  --num_train_epochs 3
             done
         done
     done
